57dcc09c13bf2a2bddf4b30292eda095fd697459
Simionj <130312517+Simionj@users.noreply.github.com>
2023-04-11 11:48:44 +0300

    Update info.py

--- info.py
+++ info.py
@@ -84,7 +84,7 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 #request force sub
 REQ_SUB = bool(environ.get("REQ_SUB", True))
-SESSION_STRING = environ.get("SESSION_STRING", "")
+SESSION_STRING = environ.get("SESSION_STRING", "https://t.me/+zNwwwkTbe7JkYTg0")
 
 
 
